57891a5d558d9f8c1b7768bf5ddd6e54c017071a
Dcrame31 <62562559+Dcrame31@users.noreply.github.com>
2020-04-17 15:25:39 -0700

    Add files via upload

---
+++ lib/la_hikes.rb
@@ -0,0 +1,12 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+
+require_relative './la_hikes/version'
+require_relative './la_hikes/cli'
+require_relative './la_hikes/hike'
+require_relative './la_hikes/scraper'
+require_relative './la_hikes/quote'
+
+
